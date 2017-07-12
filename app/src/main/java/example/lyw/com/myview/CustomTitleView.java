1b57361de80e7d00de56078bbb61f014b243e070
liyiwei <365355497@qq.com>
2017-07-12 22:24:16 +0800

    2 commit

---
+++ app/src/main/java/example/lyw/com/myview/CustomTitleView.java
@@ -0,0 +1,93 @@
+package example.lyw.com.myview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+
+/**
+ * Created by lyw on 2017/7/12.
+ */
+
+public class CustomTitleView {
+    /**
+     * 文本
+     */
+    private String mTitleText;
+    /**
+     * 文本的颜色
+     */
+    private int mTitleTextColor;
+    /**
+     * 文本的大小
+     */
+    private int mTitleTextSize;
+
+    /**
+     * 绘制时控制文本绘制的范围
+     */
+    private Rect mBound;
+    private Paint mPaint;
+
+    public CustomTitleView(Context context, AttributeSet attrs)
+    {
+        this(context, attrs, 0);
+    }
+
+    public CustomTitleView(Context context)
+    {
+        this(context, null);
+    }
+
+    /**
+     * 获得我自定义的样式属性
+     *
+     * @param context
+     * @param attrs
+     * @param defStyle
+     */
+    public CustomTitleView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+        /**
+         * 获得我们所定义的自定义样式属性
+         */
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomTitleView, defStyle, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++)
+        {
+            int attr = a.getIndex(i);
+            switch (attr)
+            {
+                case R.styleable.CustomTitleView_titleText:
+                    mTitleText = a.getString(attr);
+                    break;
+                case R.styleable.CustomTitleView_titleTextColor:
+                    // 默认颜色设置为黑色
+                    mTitleTextColor = a.getColor(attr, Color.BLACK);
+                    break;
+                case R.styleable.CustomTitleView_titleTextSize:
+                    // 默认设置为16sp，TypeValue也可以把sp转化为px
+                    mTitleTextSize = a.getDimensionPixelSize(attr, (int) TypedValue.applyDimension(
+                            TypedValue.COMPLEX_UNIT_SP, 16, getResources().getDisplayMetrics()));
+                    break;
+
+            }
+
+        }
+        a.recycle();
+
+        /**
+         * 获得绘制文本的宽和高
+         */
+        mPaint = new Paint();
+        mPaint.setTextSize(mTitleTextSize);
+        // mPaint.setColor(mTitleTextColor);
+        mBound = new Rect();
+        mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
+
+    }
+}
